7f7a25eb4fb0c9202566488d876f14614e62f2ff
dmholtz <56723830+dmholtz@users.noreply.github.com>
2022-06-14 21:30:41 +0200

    Fix stopping criterion of bidirectional dijkstra

--- pkg/graph/bidirectionalDijkstra.go
+++ pkg/graph/bidirectionalDijkstra.go
@@ -2,9 +2,12 @@ package graph
 
 import (
 	"container/heap"
+	"math"
 )
 
 func BidirectionalDijkstra(g Graph, origin, destination int) ([]int, int) {
+	// reference: https://www.homepages.ucl.ac.uk/~ucahmto/math/2020/05/30/bidirectional-dijkstra.html
+
 	dijkstraItemsForward := make([]*PriorityQueueItem, g.NodeCount(), g.NodeCount())
 	originItem := PriorityQueueItem{itemId: origin, priority: 0, predecessor: -1, index: -1}
 	dijkstraItemsForward[origin] = &originItem
@@ -12,8 +15,6 @@ func BidirectionalDijkstra(g Graph, origin, destination int) ([]int, int) {
 	dijkstraItemsBackward := make([]*PriorityQueueItem, g.NodeCount(), g.NodeCount())
 	targetItem := PriorityQueueItem{itemId: destination, priority: 0, predecessor: -1, index: -1}
 	dijkstraItemsBackward[destination] = &targetItem
-
-	visitedNodes := make([]bool, g.NodeCount(), g.NodeCount())
 
 	pqForward := make(PriorityQueue, 0)
 	heap.Init(&pqForward)
@@ -23,72 +24,86 @@ func BidirectionalDijkstra(g Graph, origin, destination int) ([]int, int) {
 	heap.Init(&pqBackward)
 	heap.Push(&pqBackward, dijkstraItemsBackward[destination])
 
-	currentNodeId := 0
-	for len(pqForward) > 0 && len(pqBackward) > 0 {
-		currentPqItem := heap.Pop(&pqForward).(*PriorityQueueItem)
-		currentNodeId = currentPqItem.itemId
+	mu := math.MaxInt // will contain the shortest distance once the loop terminates
+	middleNodeId := 0
 
-		for _, edge := range g.GetEdgesFrom(currentNodeId) {
+	for len(pqForward) > 0 && len(pqBackward) > 0 {
+		forwardPqItem := heap.Pop(&pqForward).(*PriorityQueueItem)
+		forwardNodeId := forwardPqItem.itemId
+		backwardPqItem := heap.Pop(&pqBackward).(*PriorityQueueItem)
+		backwardNodeId := backwardPqItem.itemId
+
+		// stopping criterion
+		if dijkstraItemsForward[forwardNodeId].priority+dijkstraItemsBackward[backwardNodeId].priority >= mu {
+			break
+		}
+
+		// forward search
+		for _, edge := range g.GetEdgesFrom(forwardNodeId) {
 			successor := edge.To
 
 			if dijkstraItemsForward[successor] == nil {
-				newPriority := dijkstraItemsForward[currentNodeId].priority + edge.Distance
-				pqItem := PriorityQueueItem{itemId: successor, priority: newPriority, predecessor: currentNodeId, index: -1}
+				newPriority := dijkstraItemsForward[forwardNodeId].priority + edge.Distance
+				pqItem := PriorityQueueItem{itemId: successor, priority: newPriority, predecessor: forwardNodeId, index: -1}
 				dijkstraItemsForward[successor] = &pqItem
 				heap.Push(&pqForward, &pqItem)
 			} else {
-				if updatedDistance := dijkstraItemsForward[currentNodeId].priority + edge.Distance; updatedDistance < dijkstraItemsForward[successor].priority {
+				if updatedDistance := dijkstraItemsForward[forwardNodeId].priority + edge.Distance; updatedDistance < dijkstraItemsForward[successor].priority {
 					pqForward.update(dijkstraItemsForward[successor], updatedDistance)
-					dijkstraItemsForward[successor].predecessor = currentNodeId
+					dijkstraItemsForward[successor].predecessor = forwardNodeId
 				}
+			}
+
+			if x := dijkstraItemsBackward[successor]; x != nil && dijkstraItemsForward[forwardNodeId].priority+edge.Distance+x.priority < mu {
+				mu = dijkstraItemsForward[forwardNodeId].priority + edge.Distance + x.priority
+				dijkstraItemsForward[successor].predecessor = forwardNodeId
+				middleNodeId = successor
 			}
 		}
 
-		if visitedNodes[currentNodeId] {
-			break
-		} else {
-			visitedNodes[currentNodeId] = true
-		}
-
-		currentPqItem = heap.Pop(&pqBackward).(*PriorityQueueItem)
-		currentNodeId = currentPqItem.itemId
-
-		for _, edge := range g.GetEdgesFrom(currentNodeId) {
+		// backward search
+		for _, edge := range g.GetEdgesFrom(backwardNodeId) {
 			successor := edge.To
 
 			if dijkstraItemsBackward[successor] == nil {
-				newPriority := dijkstraItemsBackward[currentNodeId].priority + edge.Distance
-				pqItem := PriorityQueueItem{itemId: successor, priority: newPriority, predecessor: currentNodeId, index: -1}
+				newPriority := dijkstraItemsBackward[backwardNodeId].priority + edge.Distance
+				pqItem := PriorityQueueItem{itemId: successor, priority: newPriority, predecessor: backwardNodeId, index: -1}
 				dijkstraItemsBackward[successor] = &pqItem
 				heap.Push(&pqBackward, &pqItem)
 			} else {
-				if updatedDistance := dijkstraItemsBackward[currentNodeId].priority + edge.Distance; updatedDistance < dijkstraItemsBackward[successor].priority {
+				if updatedDistance := dijkstraItemsBackward[backwardNodeId].priority + edge.Distance; updatedDistance < dijkstraItemsBackward[successor].priority {
 					pqBackward.update(dijkstraItemsBackward[successor], updatedDistance)
-					dijkstraItemsBackward[successor].predecessor = currentNodeId
+					dijkstraItemsBackward[successor].predecessor = backwardNodeId
 				}
 			}
-		}
 
-		if visitedNodes[currentNodeId] {
-			break
-		} else {
-			visitedNodes[currentNodeId] = true
+			if x := dijkstraItemsForward[successor]; x != nil && dijkstraItemsBackward[backwardNodeId].priority+edge.Distance+x.priority < mu {
+				mu = dijkstraItemsBackward[backwardNodeId].priority + edge.Distance + x.priority
+				dijkstraItemsBackward[successor].predecessor = backwardNodeId
+				middleNodeId = successor
+			}
 		}
 	}
 
 	length := -1           // by default a non-existing path has length -1
 	path := make([]int, 0) // by default, a non-existing path is an empty slice
-	if dijkstraItemsForward[currentNodeId] != nil {
-		length = dijkstraItemsForward[currentNodeId].priority + dijkstraItemsBackward[currentNodeId].priority
-		for nodeId := currentNodeId; nodeId != -1; nodeId = dijkstraItemsForward[nodeId].predecessor {
-			path = append([]int{nodeId}, path...)
-		}
-		if path[len(path)-1] == currentNodeId {
-			path = path[0 : len(path)-1]
-		}
-		for nodeId := currentNodeId; nodeId != -1; nodeId = dijkstraItemsBackward[nodeId].predecessor {
-			path = append(path, nodeId)
+
+	// check if path exists
+	if mu < math.MaxInt {
+		length = mu
+		// sanity check: length == dijkstraItemsForward[middleNodeId].priority + dijkstraItemsBackward[middleNodeId].priority
+		if dijkstraItemsForward[middleNodeId] != nil && dijkstraItemsBackward[middleNodeId] != nil {
+			for nodeId := middleNodeId; nodeId != -1; nodeId = dijkstraItemsForward[nodeId].predecessor {
+				path = append([]int{nodeId}, path...)
+			}
+			if path[len(path)-1] == middleNodeId {
+				path = path[0 : len(path)-1]
+			}
+			for nodeId := middleNodeId; nodeId != -1; nodeId = dijkstraItemsBackward[nodeId].predecessor {
+				path = append(path, nodeId)
+			}
 		}
 	}
+
 	return path, length
 }
