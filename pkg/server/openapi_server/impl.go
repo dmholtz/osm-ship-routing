f727c9fa51e047b54c263b44c66f683d96624fe7
dmholtz <56723830+dmholtz@users.noreply.github.com>
2022-10-07 22:18:46 +0200

    Remove legacy server

--- pkg/server/openapi_server/impl.go
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-package openapi_server
-
-//Implementation response defines an error code with the associated body
-type ImplResponse struct {
-	Code int
-	Body interface{}
-}
